4c9de37f3c56141ff941e5da766df1212f559834
Shibi7 <98649307+Shibi7@users.noreply.github.com>
2022-09-19 19:12:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
-// import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import { getMatches } from './api/Apii';
-import { Fragment, useEffect, useState } from 'react';
-import React from 'react';
-import Mycard from './components/Mycard';
-import {Grid, Typography} from '@mui/material';
+import { Grid, Typography, Box } from "@mui/material"
+import Navbar from './components/navbar';
+import MyCard from './components/card';
+import { getMatches } from './API/API';
+import React, { Fragment, useEffect, useState } from 'react';
 
 
 function App() {
-  const [matches,setMatches] = useState([]);
 
-  useEffect(() => {
-    getMatches()
-    .then((data) => {setMatches([...data.data]);
-    console.log(data.data);})
-    .catch(error => console.log("Error: ",error))
-
-  }, []);
+  const [matches, setMatches] = useState([]);
   
- 
+  useEffect(()=>{
+    getMatches()
+        .then( data => {
+          setMatches([...data.data]);
+          console.log("DATA:", data.data);
+        })
+        .catch( err => alert("Could not load the data"));
+  },[]);
+
+
   return (
     <div className="App">
       <Navbar />
-      <Typography variant = "h3" >Welcome to cricket score App</Typography>
-      <Grid container>
-        {
-        matches.map((match)=>(
-          <Fragment>
-            {
-              match.type ==="Twenty20" ?(
-                <Mycard key={match.unique_id} match = {match} />
-              )
-              :("")
-            }
-          </Fragment>
-          ))
-      }
-        </Grid>
-      
+      <Typography className='heading' variant='h3' mt="2"> Live Scoring</Typography>
+      <Typography className='heading' variant='h5' mt='2'>(t-20 matches)</Typography>
+      <Grid container justifyContent='center'>
+        <Box  className='card-box'>
+          {matches.map((match)=> (
+            <Fragment>
+              {(match.matchType==="t20")? 
+            (<MyCard key={match.id} 
+            match={match} />):("")}
+            </Fragment>
+          ))}
+        </Box>
+      </Grid>
     </div>
   );
 }
